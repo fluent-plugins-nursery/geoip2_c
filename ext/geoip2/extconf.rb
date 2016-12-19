cd97431ce480f8ed45ea03330b576319f00b64a8
okkez <okkez000@gmail.com>
2016-12-19 22:31:10 +0900

    Remove debug option

--- ext/geoip2/extconf.rb
+++ ext/geoip2/extconf.rb
@@ -4,6 +4,7 @@ dir_config("maxminddb")
 have_header("maxminddb.h")
 have_library("maxminddb")
 
-$CFLAGS << " -std=c99 -g -O0"
+$CFLAGS << " -std=c99"
+# $CFLAGS << " -g -O0"
 
 create_makefile("geoip2/geoip2")
