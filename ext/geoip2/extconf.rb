709555e43ac4a9b91602419d854e9ed91f54f133
Shizuo Fujita <fujita@clear-code.com>
2026-03-06 08:13:44 +0900

    Fix Windows build by migrating to mini_portile2
    The previous build process using `bootstrap` failed on Windows/MSYS2 environments.
    By adopting mini_portile2, the build process now downloads the release tarball
    containing the `configure` script, bypassing the need for autoreconf.

--- ext/geoip2/extconf.rb
+++ ext/geoip2/extconf.rb
@@ -1,33 +1,17 @@
 require "mkmf"
-require "rbconfig"
+require "mini_portile2"
 
-libdir = RbConfig::CONFIG["libdir"]
-includedir = RbConfig::CONFIG["includedir"]
+maxminddb_version = "1.13.3"
 
-maxminddb_dir = File.expand_path(File.join(__dir__, "libmaxminddb"))
-gem_root = File.expand_path('..', __dir__)
+recipe = MiniPortile.new("libmaxminddb", maxminddb_version)
+recipe.files = ["https://github.com/maxmind/libmaxminddb/releases/download/#{maxminddb_version}/libmaxminddb-#{maxminddb_version}.tar.gz"]
+recipe.cook
+recipe.activate
 
-if !File.directory?(maxminddb_dir) ||
-   # '.', '..', and possibly '.git' from a failed checkout:
-   Dir.entries(maxminddb_dir).size <= 3
-  Dir.chdir(gem_root) { system('git submodule update --init') } or fail 'Could not fetch maxminddb'
-end
+$INCFLAGS = "-I#{File.join(recipe.path, 'include')} " + $INCFLAGS
+$LDFLAGS << " -L#{File.join(recipe.path, 'lib')} -lmaxminddb"
+$CFLAGS << " -std=c99 -fPIC -fms-extensions -I#{File.join(recipe.path, 'include')}"
 
-Dir.chdir(maxminddb_dir) do
-  system("./bootstrap") or fail "Couldn't run maxminddb `bootstrap`"
-  system({ "CFLAGS" => "-fPIC" }, "./configure", "--disable-shared", "--disable-tests") or fail "Couldn't run maxminddb `configure`"
-  system("make", "clean") or fail "Couldn't run maxminddb `make clean`"
-  system("make") or fail "Couldn't run maxminddb `make`"
-end
-
-header_dirs = [includedir, "#{maxminddb_dir}/include"]
-lib_dirs = [libdir, "#{maxminddb_dir}/src/.libs"]
-
-dir_config("maxminddb", header_dirs, lib_dirs)
 have_func("rb_sym2str", "ruby.h")
-
-$LDFLAGS << " -L#{maxminddb_dir}/src/.libs -lmaxminddb"
-$CFLAGS << " -std=c99 -fPIC -fms-extensions -I#{maxminddb_dir}/src/.libs"
-# $CFLAGS << " -g -O0"
 
 create_makefile("geoip2/geoip2")
